cc310409dea37cca40f77cf6d867ba4a370f8233
Holden Altaffer <haltaffer@gmail.com>
2018-03-29 21:58:44 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,9 @@
 class Person
   def initialize(attributes)
+    attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
+:weight, :handed, :complexion, :t_shirt_size,
+:wrist_size, :glove_size, :pant_length, :pant_width
+
     attributes.each {|key, value| self.send(("#{key}="), value)}
   end
 end
